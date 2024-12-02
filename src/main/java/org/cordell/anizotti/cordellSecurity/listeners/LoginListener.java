cc0c179f7e5d16eb90d7d16e4feb36344e7a5494
j1sk1ss <fotikolay222@gmail.com>
2024-12-02 11:30:04 +0500

    ver 1.1

--- src/main/java/org/cordell/anizotti/cordellSecurity/listeners/LoginListener.java
+++ src/main/java/org/cordell/anizotti/cordellSecurity/listeners/LoginListener.java
@@ -5,33 +5,45 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.cordell.anizotti.cordellSecurity.CordellSecurity;
 
+import java.util.List;
+import java.util.Random;
+
 
 public class LoginListener implements Listener {
+
+    private static final String defaultMessage = "Also - this is CordellSecurity plugin here!";
+    private static final List<String> cordellSecurityList = List.of(
+            "Hello Bob!", "I`m kitty - Meow!", "You should not pass.", "Cordell - best company ever",
+            "Default denied text", "What's your name?", "Pig says oink", "Anizotti city is better then Los-Aidaros",
+            "Make sure, that your IP whitelisted, Bob!"
+    );
+
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent e) {
-        System.out.println("[AnizottiSecurity] Player " + e.getPlayer().getName() + " try to logged in");
-
-        var player = e.getPlayer();
+        var player    = e.getPlayer();
         var firstAddr = e.getAddress();
-        var ipString = firstAddr.getHostAddress();
-        var nickname = player.getName();
+        var ipString  = firstAddr.getHostAddress();
+        var nickname  = player.getName();
+
+        System.out.println("Player " + nickname + " try to logged in under host: " + ipString);
 
         try {
+            var title = cordellSecurityList.get(new Random().nextInt(cordellSecurityList.size())) + "\n" + defaultMessage;
             var ip = CordellSecurity.playerWhiteList.getString(nickname);
             if (ip == null) {
-                System.out.println("[CordellSecurity] Player IP not found!");
-                e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "Who are you?\nAlso - this is CordellSecurity plugin here!");
+                System.out.println("Player IP not found!");
+                e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST,  title);
                 return;
             }
 
             if (!ip.equals(ipString)) {
-                System.out.println("[CordellSecurity] Player IP not allowed!");
-                e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "IP is wrong for provided nickname!\nAlso - this is CordellSecurity plugin here!");
+                System.out.println("Player IP not allowed!");
+                e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, title);
             }
 
-            player.sendMessage("[Cordell Security] You are logged in under host: " + ip);
+            player.sendMessage("You are logged in under host: " + ip);
         } catch (Exception ex) {
-            e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "I don`t know you! Or I broken...\nAnyway you don`t pass.");
+            e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "I don`t know you! Or I broken...\nAnyway you don`t pass.\n" + defaultMessage);
             throw new RuntimeException(ex);
         }
     }
